738718650b59fafad3c4b2509021783461749eda
hrokr <alex.rook@gmail.com>
2017-08-16 15:53:58 -0400

    added code and noted that map() is different in Py2 vs Py3

--- FP/funct_01.md
+++ FP/funct_01.md
@@ -3,15 +3,13 @@
 ## Task and Extra Credit (if any)
     Get some practice with functional programming.
 
-
 ## Time
     start 1500 -- end 1533 (MPV)
 
     total time ==> approx. 33 min, no refactoring
 
-
 ## What I learned:
-
+    In Python2, map() returns a list; in Python3, it returns an iterator.
 
 ## Where I would go next:
     More practice!
--- FP/funct_01.py
+++ FP/funct_01.py
@@ -1,5 +1,18 @@
 # funct_01.py
 
+name_lengths = map(len, ["Mary", "Isla", "Sam"])
+
+print name_lengths
+
+# This only works in python 2. Python 3 yeilds something like:
+# <map object at 0x100b87a20>
+
+#name_lengths = map(len, ["Mary", "Isla", "Sam"])
+#print(name_lengths)
+
+# First question -- why is that?
+# In Python2, map() returns a list; in Python3, it returns an iterator.
+
 # This simple python 2 script:
 
 '''names = ['Mary', 'Isla', 'Sam']
@@ -10,13 +23,11 @@ for i in range(len(names)):
 print names
 '''
 
-
 # is functionally equilivant to this ...
 
 names = ['Mary', 'Isla', 'Sam']
 secret_names = map(hash, names)
 print secret_names
-
 
 # Let's pass in a function using the same list of names above.
 
@@ -25,7 +36,6 @@ def double(name):
 
 doubled_names = map(double, names)
 print "doubled is", doubled_names
-
 
 # Now let try it using a simple lambda
 
