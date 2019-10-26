79c008cf469461513c8a8afceba2e5648d8e80fc
Alex Rook <alex.rook@gmail.com>
2019-10-25 21:42:50 -0600

    Add: evaluation of beta kata

---
+++ CodeWars/averaging the array 8b.py
@@ -0,0 +1,26 @@
+def Averaging the Array(arr):
+    """
+Averaging the Array <beta -- estimated at 8 kyu>
+
+Comments:
+    I set this as kyu 8 as it was super simple
+
+Task:
+    Define a function that finds the average of all the numbers in the array given.
+    Remember, the output will only be a float if one of the numbers being used to
+    compute it is also a float.
+
+    return sum(arr)/len(arr)
+    
+
+
+Input >> Output Examples
+    listAvg([6,12,36,30]) # => 21.0
+    listAvg([12,6,9,4,3,7]) # => 6.833333333333333  
+    
+    """
+
+"""
+Other solutions from CW:
+    n/a
+"""
